b945c48226a95cd1bebb116d69fc5a184751e35b
Unknown <jakob.d957@stu.nebo.edu>
2018-09-12 10:27:10 -0600

    who

---
+++ 2D Game/Assets/Scripts/Playercontrol.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontrol : MonoBehaviour {
+
+
+    // Player movement variables
+    public int MoveSpeed;
+    public float JumpHeight;
+
+
+    // player grounded variables
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+     void FixedUpdate() {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		//this code makes the charcter jump
+        if(Input.GetKeyDown (KeyCode.W)&& grounded){
+            Jump();
+        }
+        //coad maes tehc haracter move from side to side
+        if (Input.GetKey (KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        }
+        if (Input.GetKey (KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y); 
+        }
+    }
+    public void Jump(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+    }
+}
+
