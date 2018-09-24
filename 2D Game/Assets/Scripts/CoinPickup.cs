a9382008390c19e853637a861528a15bdd190f81
Unknown <jakob.d957@stu.nebo.edu>
2018-09-24 10:08:36 -0600

    kinda death box and coin

---
+++ 2D Game/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CoinPickup : MonoBehaviour {
+
+    public int pointsToAdd;
+
+	// Use this for initialization
+	void OnTriggerenter2D (Collider2D other) 
+    {
+        if (other.GetComponent<Rigidbody2D> () == null)
+            return;
+
+        ScoreManager.AddPoints(pointsToAdd);
+        //Score += 
+
+        Destroy (gameObject);
+
+		
+	}
+}
