436ca7d6e5d91ef3632926df66ab12a597c593fe
Unknown <jakob.d957@stu.nebo.edu>
2018-09-26 10:26:12 -0600

    iffy enemy

---
+++ 2D Game/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    //movement variable
+
+    public float MoveSpeed;
+    public bool MoveRight;
+
+    //wall chec
+    public Transform WallCheck;
+
+    public float WallCheckRadius;
+
+    public LayerMask WhatIsWall;
+
+    private bool HittingWall;
+
+    private bool NotAtEgde;
+
+    public Transform EdgeCheck;
+
+    // Update is called once per frame
+    void Update()
+    {
+        NotAtEgde = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+
+        HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+
+        if (HittingWall || !NotAtEgde)
+        {
+            MoveRight = !MoveRight;
+        }
+
+        if (MoveRight)
+        {
+            transform.localScale = new Vector3(-4f, 4f, 0f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else
+        {
+            transform.localScale = new Vector3(4f, 4f, 0f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}
