0c3cd163e2109e271bf7473533b7aa1c7702947a
Unknown <jakob.d957@stu.nebo.edu>
2018-11-28 10:28:33 -0700

    Main Menu

---
+++ 2D Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    // Use this for initialization
+    public int levelToLoad;
+
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+
+    }
+    public void LevelExit()
+    {
+        Application.Quit();
+    }
+}
